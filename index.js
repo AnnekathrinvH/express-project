d9a9e4887af2a62bc7f90ee44c85b9ff5ce64d7d
AnnekathrinvH <annevonhacht@web.de>
2016-08-25 17:31:28 +0200

    updated server for twitter request and projects page

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
-var userName;
 
+var userName;
 var getProjects = require('./getProjects');
-var myProjectsObject = getProjects.getProjects();
-console.log(myProjectsObject);
+var myProjects = getProjects.getProjects();
+
+var twitterRequest = require('./TwitterRequest');
+twitterRequest.twitterTokenRequest();
 
 app.use(require('body-parser').urlencoded({
     extended: false
@@ -13,45 +15,90 @@ app.use(require('body-parser').urlencoded({
 
 app.use(require('cookie-parser')());
 
-app.get('/getaname', function(req, res) {
-    res.sendFile(__dirname + '/projects/name/index.html');
-    app.use(express.static(__dirname + '/projects'));
-
-});
-
 var hb = require('express-handlebars');
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
 
 
-app.all('*', function(req, res, next) {
+
+
+app.use(function(req, res, next) {
     userName = req.cookies.name;
-    if (req.url === '/name') {
-        next();
-    }
-    if ((userName === undefined || userName === ' ') && req.url != '/getaname') {
-        console.log('redirected');
-        res.redirect('/getaname');
-    }
+
+    console.log('user name:'+ userName);
+    console.log(req.url);
+
     if (userName) {
         console.log('name there');
         next();
     }
 
+    if (userName === undefined && (req.url === '/name' || req.url === '/getaname')) {
+        console.log('go on to name');
+        next();
+    }
+    if ((userName === undefined) && req.url != '/getaname') {
+        console.log('redirected');
+        res.redirect('/getaname');
+        next();
+    }
 });
 
 app.use(express.static(__dirname + '/projects'));
 
 
-app.post('/name', function(req, res) {
-    var fullName = req.body.first + ' '+ req.body.last;
-    res.cookie('name', fullName);
-    console.log('Cookies: ', req.cookies);
-    res.redirect('hello/world');
+
+app.get('/getaname', function(req, res) {
+    console.log('getaname!');
+    res.sendFile(__dirname + '/projects/name/index.html');
 });
 
-app.get('/hello/world', function(req, res) {
-    res.render('projectsPage', myProjectsObject);
+app.get('/twitter', function(req,res) {
+    twitterRequest.twitterDataRequest(function(data) {
+        res.send(data);
+        console.log(data);
+    });
+
+});
+
+app.post('/name', function(req, res) {
+    console.log('name page');
+    if (req.body.first && req.body.last) {
+        var fullName = req.body.first + ' '+ req.body.last;
+        res.cookie('name', fullName);
+        res.redirect('myprojects');
+
+    } else {
+        userName = undefined;
+        res.redirect('getaname');
+    }
+    console.log('Cookies: ', req.cookies);
+});
+
+app.get('/myprojects', function(req, res) {
+    console.log(myProjects);
+    res.render('projectsPage', {
+        myProjects
+    });
+});
+
+app.get('/:project/description', function(req, res) {
+    req.params.project;
+    console.log('projects!!');
+    for (var i = 0; i < myProjects.length; i++) {
+
+        if (myProjects[i].name === req.params.project) {
+
+            var directory = './projects/' + req.params.project + '/' + req.params.project + '.json';
+            var jsonDescription = require(directory);
+            console.log(jsonDescription);
+            res.render('projectsDescription', {
+                jsonDescription: jsonDescription,
+                layout: 'layout'
+            });
+        }
+    }
+
 });
 
 app.listen(8080);
