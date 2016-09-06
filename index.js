ef682bb9e3b64f284d19c4d9722d0349075180f8
AnnekathrinvH <annevonhacht@web.de>
2016-09-06 16:48:52 +0200

    finished node postgres exercise

--- index.js
+++ index.js
@@ -24,27 +24,37 @@ var util = require('util');
 
 var getPassword = require('./credentials.json');
 var dbPassword = getPassword.dbPassword;
+console.log(dbPassword);
+
+var redis = require('redis');
+var client = redis.createClient({
+    host: 'localhost',
+    port: 6379
+});
+
+client.on('error', function(err) {
+    console.log(err);
+});
 
 
 
 app.use(function(req, res, next) {
     userName = req.cookies.name;
-
-    console.log(req.url);
+    console.log('user name: '+userName);
+    console.log('url: '+req.url);
 
     if (userName) {
         console.log('name there');
-        next();
+        return next();
     }
-
     if (userName === undefined && (req.url === '/name' || req.url === '/getaname')) {
         console.log('go on to name');
-        next();
+        return next();
     }
     if ((userName === undefined) && req.url != '/getaname') {
         console.log('redirected');
         res.redirect('/getaname');
-        next();
+        return next();
     }
 });
 
@@ -64,35 +74,43 @@ app.get('/twitter', function(req,res) {
 });
 
 app.post('/name', function(req, res) {
+    console.log('redirected to name');
     var userData = {};
     var newId;
-    if (req.body.first && req.body.last) {
+    var cookieData;
 
+    if (req.body.first && req.body.last) {
+        console.log('if '+req.body.first);
         var client = new pg.Client('postgres://postgres:'+dbPassword+'@localhost:5432/users');
         client.connect();
-
         var first = req.body.first.toUpperCase();
         var last = req.body.last.toUpperCase();
 
         var query = 'INSERT INTO user_names (first_name, last_name) VALUES ($1, $2) RETURNING id';
         client.query(query, [first, last], function(err, results) {
-            newId = results.rows[0].id;
-            userData.first = first;
-            userData.last = last;
-            userData.dbid = newId;
-            var cookieData = JSON.stringify(userData);
-            res.cookie('name', cookieData);
-            console.log(userData);
+            if (err) {
+                console.log(err);
+            } else {
+                newId = results.rows[0].id;
+                userData.first = first;
+                userData.last = last;
+                userData.dbid = newId;
+                cookieData = JSON.stringify(userData);
+                console.log(cookieData);
 
+            }
             client.end();
+            res.cookie('name', cookieData);
             res.redirect('moreUserData');
-
         });
+
     } else {
+        console.log('paranoia');
         userName = undefined;
         res.redirect('getaname');
     }
-    console.log('Cookies: ', req.cookies);
+
+
 });
 
 
@@ -114,23 +132,69 @@ app.post('/moreUserData', function(req, res) {
         res.redirect('users');
     });
 
+
+
 });
 
 app.get('/users', function(req, res) {
     console.log('Cookies: ', req.cookies);
+    var colorArray = [];
+    var cityArray = [];
+    var selectedColor = req.query.color;
+    var selectedCity = req.query.city;
+    var allData;
 
+    client.get('userData', function(err, reply){
+        if (reply === null) {
+
+        }
+    })
     var client = new pg.Client('postgres://postgres:'+dbPassword+'@localhost:5432/users');
     client.connect();
     var query = 'SELECT * FROM user_names JOIN user_profile ON user_names.id=user_profile.id';
     client.query(query, function(err, results) {
-        var allData = results.rows;
+        allData = results.rows;
+
+        getUniques('color', colorArray);
+        getUniques('city', cityArray);
+
+        function getUniques(selected, myArray) {
+            var unique = {};
+            for (var i = 0; i < allData.length; i++) {
+                var key = allData[i][selected];
+                unique[key] = selected;
+            }
+            for (var colorKey in unique) {
+                var entry = {};
+                entry[selected] = colorKey;
+                myArray.push(entry);
+            }
+        }
+
+        if (selectedColor && selectedCity === undefined) {
+            allData = allData.filter(function(obj) {
+                return obj.color === selectedColor;
+            });
+        }
+        if (selectedCity && selectedColor === undefined) {
+            allData = allData.filter(function(obj) {
+                return obj.city === selectedCity;
+            });
+        }
+        if (selectedCity && selectedColor) {
+            allData = allData.filter(function(obj) {
+                return obj.color === selectedColor && obj.city === selectedCity;
+            });
+        }
+
         res.render('userDataTable', {
+            colorArray: colorArray,
+            cityArray: cityArray,
             allData: allData
         });
         client.end();
-
-        });
     });
+});
 
 
 
@@ -146,7 +210,6 @@ app.get('/:project/description', function(req, res) {
     for (var i = 0; i < myProjects.length; i++) {
 
         if (myProjects[i].name === req.params.project) {
-
             var directory = './projects/' + req.params.project + '/' + req.params.project + '.json';
             var jsonDescription = require(directory);
             console.log(jsonDescription);
